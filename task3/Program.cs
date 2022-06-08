cf563ff2146013a8ee635a6e22442829c7129dcc
«Ваше <pankova.en@gmail.com>
2022-06-08 21:53:24 +0700

    task square

---
+++ task3/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая будет выдавать 
+//название дня недели по заданному номеру.
+//3 -> Среда
+//5 -> Пятница
+
+Console.Write("Введите цифрой день недели: ");
+string NumberDay = Console.ReadLine();
+
+if(int.Parse(NumberDay) == 3)
+{
+    Console.WriteLine("среда");
+}
+else
+{
+    if(int.Parse(NumberDay) == 5)
+    {
+        Console.WriteLine("пятница");
+    }
+    else
+    {
+        if(int.Parse(NumberDay) == 1)
+        {
+            Console.WriteLine("понедельник");
+        }
+    }
+    
+// Console.WriteLine("не среда и не пятница");
+}
