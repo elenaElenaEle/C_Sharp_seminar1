1d665fd3ffd6be2a541911e29c3ebd69a76c803a
«Ваше <pankova.en@gmail.com>
2022-06-08 23:29:38 +0700

    DZ seminar 1 task1

---
+++ DZ_task1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = ; b = 7 -> max = 7, min = 5
+
+Console.Write ("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write ("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if( a > b)
+{
+    Console.WriteLine($"max = {a}");
+    Console.WriteLine($"min = {b}");
+}
+else 
+{
+    Console.WriteLine($"max = {b}");
+    Console.WriteLine($"max = {a}");
+}
